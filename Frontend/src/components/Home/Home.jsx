f2f372b23a3c240639716126f6d50b2da2b2aa50
Greeshma Medam <greeshma.medam@gmail.com>
2023-04-12 16:15:48 +0530

    render product

--- Frontend/src/components/Home/Home.jsx
+++ Frontend/src/components/Home/Home.jsx
@@ -1,22 +1,22 @@
-import { Box, Container } from "@mui/system";
+import { Box, Container, Paper, Avatar } from "@mui/material";
 import ItemCard from "../UI/Card";
 import classes from "./home.module.css";
-import { Typography,Backdrop,CircularProgress } from "@mui/material";
-import {useEffect} from 'react'
-import {getCategories,getProducts} from "../../actions/home"
-import { useDispatch,useSelector } from "react-redux";
+import { Typography, Backdrop, CircularProgress } from "@mui/material";
+import { useEffect } from "react";
+import { getCategories, getProducts } from "../../actions/home";
+import { useDispatch, useSelector } from "react-redux";
 
 const Home = () => {
-  const dispatch = useDispatch()
+  const dispatch = useDispatch();
+  useSelector((state) => state.home);
 
-  const { loading, error, products,categories } = useSelector(
+  useEffect(() => {
+    dispatch(getCategories());
+    dispatch(getProducts());
+  }, []);
+  const { loading, error, categories, products } = useSelector(
     (state) => state.home
   );
-
-  useEffect(()=>{
-    dispatch(getCategories())
-    dispatch(getProducts())
-  },[])
 
   if (loading)
     return (
@@ -24,27 +24,43 @@ const Home = () => {
         <CircularProgress />
       </Backdrop>
     );
-  else if(products && categories)
+  else if (categories && products)
     return (
-    <Box>
-      <Box className={classes.homeHeader}>
-        <img
-          src="https://images.unsplash.com/photo-1528698827591-e19ccd7bc23d?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1176&q=80"
-          alt=""
-        />
-      </Box>
-      <Container sx={{padding: "2rem 0"}}>
-        <Typography>Recommendations</Typography>
-        <Box maxWidth="lg" className={classes.homeContainer}>
-          <ItemCard />
-          <ItemCard />
-          <ItemCard />
-          <ItemCard />
-          <ItemCard />
-          <ItemCard />
+      <Box>
+        <Box className={classes.homeHeader}>
+          <img
+            src="https://images.unsplash.com/photo-1528698827591-e19ccd7bc23d?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1176&q=80"
+            alt=""
+          />
         </Box>
-      </Container>
-    </Box>
-  );
+
+        <Container sx={{ padding: "2rem 0" }}>
+          <Typography variant="h5" fontWeight={600}>
+            Choose Categories
+          </Typography>
+          <Box className={classes.categories}>
+            {categories.map((cat) => (
+              <Paper
+                elevation={0}
+                variant="outlined"
+                className={classes.catCard}
+              >
+                <Avatar alt="Remy Sharp" src={cat.img} variant="sqaure" />
+                <Typography>{cat.name}</Typography>
+              </Paper>
+            ))}
+          </Box>
+
+          <Typography variant="h5" fontWeight={600}>
+            Recommendations
+          </Typography>
+          <Box maxWidth="lg" className={classes.homeContainer}>
+            {products.results.map((prod) => (
+              <ItemCard prod={prod} />
+            ))}
+          </Box>
+        </Container>
+      </Box>
+    );
 };
 export default Home;
--- Frontend/src/components/UI/Card.jsx
+++ Frontend/src/components/UI/Card.jsx
@@ -8,48 +8,46 @@ import CardActions from "@mui/material/CardActions";
 import Avatar from "@mui/material/Avatar";
 import IconButton from "@mui/material/IconButton";
 import Typography from "@mui/material/Typography";
-import { red } from "@mui/material/colors";
+import { yellow } from "@mui/material/colors";
 import FavoriteIcon from "@mui/icons-material/Favorite";
 import MoreVertIcon from "@mui/icons-material/MoreVert";
 import { CardActionArea } from "@mui/material";
 import { useNavigate } from "react-router";
 
-export default function ItemCard() {
+export default function ItemCard({ prod }) {
   const navigate = useNavigate();
 
   const cardClickHandler = (id) => {
     console.log("card clicked");
-    navigate("/product")
+    navigate(`/product/${prod.id}`);
   };
 
   return (
-    <Card sx={{ maxWidth: 345 }}>
+    <Card sx={{ maxWidth: 345 }} variant="outlined" elevation={0}>
       <CardActionArea onClick={cardClickHandler}>
         <CardHeader
           avatar={
-            <Avatar sx={{ bgcolor: red[500] }} aria-label="recipe">
-              R
-            </Avatar>
+            prod.owner.profile_pic ? (
+              <Avatar src={`http://127.0.0.1:8000${prod.owner.profile_pic}`} />
+            ) : (
+              <Avatar sx={{ bgcolor: yellow[400] }} aria-label="recipe">
+                R
+              </Avatar>
+            )
           }
-          title="Shrimp and Chorizo Paella"
-          subheader="September 14, 2016"
+          title={prod.owner.name}
         />
         <CardMedia
           component="img"
           height="194"
-          image="https://picsum.photos/id/1/200/300"
+          image={`http://127.0.0.1:8000${prod.img}`}
           alt="Paella dish"
         />
         <CardContent>
           <Typography fontWeight={700} variant="body1">
-            Rs 100000
+            Rs {prod.price}
           </Typography>
-          <Typography variant="body1">Brand Name</Typography>
-          <Typography variant="body2" color="text.secondary">
-            This impressive paella is a perfect party dish and a fun meal to
-            cook together with your guests. Add 1 cup of frozen peas along with
-            the mussels, if you like.
-          </Typography>
+          <Typography variant="body1">{prod.price}</Typography>
         </CardContent>
       </CardActionArea>
       <CardActions
