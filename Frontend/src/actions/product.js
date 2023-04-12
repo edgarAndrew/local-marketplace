60f1384781802c0ea692a0c21e2f9baaf571f25c
edgarAndrew <edgarcamelo03@gmail.com>
2023-04-12 17:28:44 +0530

    new changes

--- Frontend/src/actions/product.js
+++ Frontend/src/actions/product.js
@@ -19,3 +19,131 @@ export const getProduct = (id)=>async(dispatch)=>{
         })
     }    
 }
+
+export const sellElectronics = (img,title,desc,catId,date,price) => async (dispatch) => {
+    try {
+        dispatch({
+            type: "SellElectronicsRequest",
+        });
+        let bodyFormData = new FormData();
+        bodyFormData.append("img", img);
+        bodyFormData.append("title", title);
+        bodyFormData.append("description", desc);
+        bodyFormData.append("category", catId);
+        bodyFormData.append("date_of_purchase", date);
+        bodyFormData.append("price", price);
+
+        const { data } = await axios.post("/api/add-product/",bodyFormData,{
+            headers: { "Content-Type": "multipart/form-data" },
+            }
+        );
+
+        dispatch({
+            type: "SellElectronicsSuccess",
+            payload: data.message,
+        });
+    } 
+    catch (error) {
+      console.log(error.response.data, error.response.status);
+      dispatch({
+        type: "SellElectronicsFailure",
+        payload: error.response.data,
+      });
+    }
+  };
+
+  export const sellProperty = (img,title,desc,catId,date,price) => async (dispatch) => {
+    try {
+        dispatch({
+            type: "SellPropertyRequest",
+        });
+        let bodyFormData = new FormData();
+        bodyFormData.append("img", img);
+        bodyFormData.append("title", title);
+        bodyFormData.append("description", desc);
+        bodyFormData.append("category", catId);
+        bodyFormData.append("date_of_purchase", date);
+        bodyFormData.append("price", price);
+
+        const { data } = await axios.post("/api/add-product/",bodyFormData,{
+            headers: { "Content-Type": "multipart/form-data" },
+            }
+        );
+
+        dispatch({
+            type: "SellPropertySuccess",
+            payload: data.message,
+        });
+    } 
+    catch (error) {
+      console.log(error.response.data, error.response.status);
+      dispatch({
+        type: "SellPropertyFailure",
+        payload: error.response.data,
+      });
+    }
+  };
+
+  export const sellFurniture = (img,title,desc,catId,date,price) => async (dispatch) => {
+    try {
+        dispatch({
+            type: "SellFurnitureRequest",
+        });
+        let bodyFormData = new FormData();
+        bodyFormData.append("img", img);
+        bodyFormData.append("title", title);
+        bodyFormData.append("description", desc);
+        bodyFormData.append("category", catId);
+        bodyFormData.append("date_of_purchase", date);
+        bodyFormData.append("price", price);
+
+        const { data } = await axios.post("/api/add-product/",bodyFormData,{
+            headers: { "Content-Type": "multipart/form-data" },
+            }
+        );
+
+        dispatch({
+            type: "SellFurnitureSuccess",
+            payload: data.message,
+        });
+    } 
+    catch (error) {
+      console.log(error.response.data, error.response.status);
+      dispatch({
+        type: "SellFurnitureFailure",
+        payload: error.response.data,
+      });
+    }
+  };
+
+  export const sellVehicle = (img,title,desc,catId,date,price) => async (dispatch) => {
+    try {
+        dispatch({
+            type: "SellVehicleRequest",
+        });
+        let bodyFormData = new FormData();
+        bodyFormData.append("img", img);
+        bodyFormData.append("title", title);
+        bodyFormData.append("description", desc);
+        bodyFormData.append("category", catId);
+        bodyFormData.append("date_of_purchase", date);
+        bodyFormData.append("price", price);
+
+        const { data } = await axios.post("/api/add-product/",bodyFormData,{
+            headers: { "Content-Type": "multipart/form-data" },
+            }
+        );
+
+        dispatch({
+            type: "SellVehicleSuccess",
+            payload: data.message,
+        });
+    } 
+    catch (error) {
+      console.log(error.response.data, error.response.status);
+      dispatch({
+        type: "SellVehicleFailure",
+        payload: error.response.data,
+      });
+    }
+  };
--- Frontend/src/reducers/product.js
+++ Frontend/src/reducers/product.js
@@ -15,6 +15,56 @@ export const productReducer = createReducer(initialState,{
         state.error = action.payload
         state.loading = false 
     },
+
+    SellElectronicsRequest:(state,action)=>{
+        state.loading = true
+    },
+    SellElectronicsSuccess:(state,action)=>{
+        state.message = action.payload
+        state.loading = false        
+    },
+    SellElectronicsFailure:(state,action)=>{
+        state.error = action.payload
+        state.loading = false 
+    },
+
+    SellPropertyRequest:(state,action)=>{
+        state.loading = true
+    },
+    SellPropertySuccess:(state,action)=>{
+        state.message = action.payload
+        state.loading = false        
+    },
+    SellPropertyFailure:(state,action)=>{
+        state.error = action.payload
+        state.loading = false 
+    },
+
+    SellFurnitureRequest:(state,action)=>{
+        state.loading = true
+    },
+    SellFurnitureSuccess:(state,action)=>{
+        state.message = action.payload
+        state.loading = false        
+    },
+    SellFurnitureFailure:(state,action)=>{
+        state.error = action.payload
+        state.loading = false 
+    },
+
+    SellVehicleRequest:(state,action)=>{
+        state.loading = true
+    },
+    SellVehicleSuccess:(state,action)=>{
+        state.message = action.payload
+        state.loading = false        
+    },
+    SellVehicleFailure:(state,action)=>{
+        state.error = action.payload
+        state.loading = false 
+    },
+
+
     clearErrors:(state,action)=>{
         state.error = null
         state.message = null
